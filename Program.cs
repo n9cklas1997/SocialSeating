8f1b37beeb648fec108a613a864cc4bb416a6d15
n9cklas1997 <165781290+n9cklas1997@users.noreply.github.com>
2024-04-02 12:43:56 +0200

    Hello World Test

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            Console.ReadKey();
+        }
+    }
+}
+
